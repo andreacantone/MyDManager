29fb29518c5d3dcbcd13963b67df1450c663c4d9
VincenzoDeRosa <vincenzo.de.rosa.93gmail.com>
2017-02-01 21:19:25 +0100

    Initial Commit

---
+++ My Diabetic Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Diabetic Manager
+//
+//  Created by Vincenzo De Rosa on 01/02/17.
+//  Copyright © 2017 Vincenzo De Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
