01839c5be8a20d7fd884edf038d10e1bc8a78226
raffaeledeluca <raffaeledeluca92@gmail.com>
2017-02-02 18:43:22 +0100

    nun so

--- My Diabetic Manager/RegisterViewController.swift
+++ My Diabetic Manager/RegisterViewController.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+struct User_profile{
+    var username: String
+    var password: String
+    var email: String
+    var name: String
+    var surname: String
+    var birthdata: String
+    var ratio_cho: Float
+    
+
+}
+
+var User_profiles: [User_profile] = []
+
+
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var userEmailTextField: UITextField!
